6e88c812f3bb08e96ddfe25b7fd40adbef0089ce
JulianHill <jul.hillebrand@gmail.com>
2014-03-21 23:32:16 +0100

    Init

--- R/TwitterAuth.R
+++ R/TwitterAuth.R
@@ -1,7 +1,27 @@
-TwitterAuth <- function(){
+TwitterAuth <- function(api_key,api_secret){
   
+  instant_pkgs(c("twitteR","RCurl"))
+
+
+
+  #library(RCurl)
+
+# Set SSL certs globally
+options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
+
+
+reqURL <- "https://api.twitter.com/oauth/request_token"
+accessURL <- "https://api.twitter.com/oauth/access_token"
+authURL <- "https://api.twitter.com/oauth/authorize"
+apiKey <- api_key
+apiSecret <- api_secret
+
+twitCred <- OAuthFactory$new(consumerKey=apiKey,consumerSecret=apiSecret,requestURL=reqURL,accessURL=accessURL,authURL=authURL)
+
+twitCred$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+
+registerTwitterOAuth(twitCred)
   
+
   
-  
-  #return(object)
 }
