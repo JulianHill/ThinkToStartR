cc8ba8621835944ee4cb5f98269721a9049e413a
JulianHill <jul.hillebrand@gmail.com>
2014-06-20 14:35:56 +0200

    test

--- ThinkToStartR/inst/tests/test-twitter.R
+++
@@ -1,9 +0,0 @@
-context("Twitter")
-
-test_that("Clean.text function works", {
-  
-  test_string <- "I am a Test String! @!"
-  
-  expect_equal(clean.text(test_string),"i am a test string")
-  
-}
